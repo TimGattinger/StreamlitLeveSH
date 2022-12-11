f36469844b8a7acca73abf54606a968a5ce490e7
TimGattinger <99266389+TimGattinger@users.noreply.github.com>
2022-12-11 21:34:18 +0100

    Add files via upload

---
+++ 2_Zeitreihe.py
@@ -0,0 +1,145 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from streamlit_folium import st_folium
+import folium
+import csv
+
+st.title("Zeitreihe")
+st.sidebar.info('Diese App zeigt Pegelstände von Schleswig Holstein und deren statistische Auswertung. Aktuell und als Zeitreihe. Tim Gattinger 12/2022')
+st.sidebar.success('Quelle http://www.umweltdaten.landsh.de')
+st.sidebar.write("Pegel " +  st.session_state["my_input"] + " ausgewählt")
+
+try:
+
+
+
+    Ort = st.session_state["my_input"]
+    query = f"name=='{Ort}'"
+
+    df = pd.read_pickle("PegelMetadatenStats.pkl")
+    df_f = df.query(query)
+    #st.write(df_f)
+    t_ID = df_f['ID'].values[0]
+    ### anedern wenn online als app/Github
+    #t = r"C:\Users\User\_pegel_Atom\Pegelab1900\\" + str(t_ID) + ".pkl"
+    t = r"C:\Users\User\_Pegel_Atom_CleanCode\PegelAb1900_Test_1\\" + str(t_ID) + ".pkl"
+    Pegeltitle = "Pegel " + str(Ort) + ", ID: " + str(t_ID)
+    df = pd.read_pickle(t)
+    
+
+    oldest = df['date'].min()
+    old = str(oldest)
+    old = old[0:4]
+
+    ####TimeSlider Start Time selector
+    from datetime import date
+
+    current_year = date.today().year
+    slider = st.slider('Wähle ein Start Jahr ', min_value=int(old), value=2012 ,max_value=current_year, step=1)#
+
+    startS = str(slider) + "-1-1"
+    endS = str(current_year) + "-12-31"
+
+    mask = (df['date'] > startS) & (df['date'] <= endS)
+    df = df.loc[mask]
+
+    tab1, tab2, tab3 = st.tabs(["Graph", "Histogramm", "Statistiken"])
+
+
+    with tab1:
+    #st.dataframe(df.head())
+    # Create figure
+        fig = go.Figure()
+
+        fig.add_trace(
+            go.Scatter(x=list(df.date), y=list(df.level), line_shape='spline'))
+            #go.Scatter(df, x='date', y='level', line_shape='spline'))
+
+        fig.add_hline(y=df_f['MW10'].values[0], line_color='black', line_dash='dash', annotation_text="MW10")
+        fig.add_hline(y=df_f['MQ100'].values[0], line_color='red', line_dash='dash', annotation_text="Hundertjähriges Hochwasser (MQ100)")
+        fig.add_hrect(y0=df_f['MNW10'].values[0], y1=df_f['MHW10'].values[0], line_width=0, fillcolor="black", opacity=0.2)
+
+        # Set title
+
+        fig.update_layout(
+            title_text=Pegeltitle
+        )
+
+        # Add range slider
+        fig.update_layout(width=900,height=600,
+            xaxis=dict(title='Datum',
+
+                rangeselector=dict(
+                    buttons=list([
+                        dict(count=1,
+                             label="1m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=6,
+                             label="6m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=1,
+                             label="YTD",
+                             step="year",
+                             stepmode="todate"),
+                        dict(count=1,
+                             label="1y",
+                             step="year",
+                             stepmode="backward"),
+                        dict(step="all")
+                    ])
+                ),
+
+                rangeslider=dict(
+                    visible=True
+                ),
+                type="date"
+            )
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab2:
+
+        level = [df['level']]
+
+        group_labels = ['Pegelstände [cm PN]'] # name of the dataset
+
+        fig2 = ff.create_distplot(level, group_labels)
+        fig2.update_layout(
+            title_text=Pegeltitle
+        )
+
+        st.plotly_chart(fig2, use_container_width=True)
+
+    with tab3:
+
+
+
+        fig = px.box(df, x ='year', y ='level', title="Pegelstände gruppiert nach Jahr von " + str(slider) + " bis " + str(current_year),
+
+        labels={
+                     "year": "Jahr",
+                     "level": "Pegelstand"
+
+                 }
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+        fig = px.box(df, x ='month', y ='level', title="Pegelstände gruppiert nach Monat von " + str(slider) + " bis " + str(current_year),
+        labels={
+                     "month": "Monat",
+                     "level": "Pegelstand"
+
+                 }
+        )
+        st.plotly_chart(fig, use_container_width=True)
+        st.table(df.describe())
+
+except:
+    st.write("Bitte wählen Sie einen Pegel auf der Hauptseite")
---
+++ 3_3D_Karte.py
@@ -0,0 +1,116 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+from itertools import cycle
+import random
+import time
+import pydeck
+
+st.sidebar.info('Diese App zeigt Pegelstände von Schleswig Holstein. Aktuell und historisch. Tim Gattinger 12/2022')
+st.sidebar.success('Quelle http://www.umweltdaten.landsh.de')
+st.sidebar.write("Pegel " +  st.session_state["my_input"] + " ausgewählt")
+
+#bicycle_counts = pd.read_pickle("./3D_Pydeck_normalisedMQ100.pkl")
+bicycle_counts = pd.read_pickle("./3D_Pydeck_normalisedMQ100_2017_2022.pkl")
+counts_df = pd.read_pickle("./3D_Pydeck_BasicDataOBJ.pkl")
+
+
+# don't modify the outputs from a cached streamlit function
+counts_df = counts_df.copy()
+
+# resample daily data to monthly means
+bicycle_counts = bicycle_counts.resample("M").mean()
+years_months_values = [(d.year, d.month) for d in bicycle_counts.index]
+year, month = years_months_values[0]
+
+# Setup presentation widgets
+st.header("Visualisierung der Pegelstände 2020-2022 in Schleswig-Holstein")
+date_value = st.empty()
+month_slider = st.empty()
+animations = {"Stop": None, "Langsam": 0.4, "Mittel": 0.2, "Schnell": 0.05}
+animate = st.radio("", options=list(animations.keys()), index=2)
+
+animation_speed = animations[animate]
+deck_map = st.empty()
+
+
+def render_slider(year, month):
+    key = random.random() if animation_speed else None
+
+    month_value = month_slider.slider(
+        "",
+        min_value=0,
+        max_value=len(years_months_values),
+        value=years_months_values.index((year, month)),
+        format="",
+        key=key,
+    )
+    year, month = years_months_values[month_value]
+    d = date(year, month, 1)
+    date_value.subheader(f"Datum: {d:%Y}-{d:%m}")
+    return year, month
+
+
+def render_map(year, month):
+    mask = (bicycle_counts.index.year == year) & (bicycle_counts.index.month == month)
+    month_counts = bicycle_counts[mask].transpose().reset_index()
+    month_counts.rename(
+        columns={
+            month_counts.columns[0]: "ID",
+            month_counts.columns[1]: "month_counts",
+        },
+        inplace=True,
+    )
+
+    counts_df["counts"] = counts_df.merge(
+        month_counts, left_on="ID", right_on="ID"
+    )["month_counts"]
+
+    display_counts = counts_df[~pd.isna(counts_df["counts"])]
+    display_counts = display_counts.round(decimals = 1)
+    #st.write(display_counts.head())
+
+    deck_map.pydeck_chart(
+        pydeck.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",#'mapbox://styles/mapbox/outdoors-v12', #"mapbox://styles/mapbox/light-v9",
+            initial_view_state=pydeck.ViewState(
+                latitude=display_counts.lat.mean(),
+                longitude=display_counts.lon.mean(),
+                zoom=7.5,
+                pitch=50,
+
+            ), tooltip=tooltip,
+            layers=[
+                pydeck.Layer(
+                    "ColumnLayer",
+                    data=display_counts,
+                    disk_resolution=12,
+                    radius=900,
+                    elevation_scale=150,
+                    get_position="[lon, lat]",
+                    get_color="[5, 25, 245]",#"[40, counts / 5000 * 255, 40, 150]",
+                    get_elevation="[counts]",
+                    pickable=True
+
+                ),
+            ],
+
+        )
+    )
+
+
+#display_counts['Fcounts'] = display_counts['counts'].apply(lambda d: f'{round(d, 2):,}')
+tooltip={
+"html": "Station <b>{name}</b> ({ID}): <b>{counts}</b> % vom hundertjährigen Hochwasser MQ100",
+"style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+}
+
+
+if animation_speed:
+    for year, month in cycle(years_months_values):
+        time.sleep(animation_speed)
+        render_slider(year, month)
+        render_map(year, month)
+else:
+    year, month = render_slider(year, month)
+    render_map(year, month)
